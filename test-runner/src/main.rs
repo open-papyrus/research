6aff6a99f983538a4a1fa8c545bc42021be5e53a
erri120 <erri120@protonmail.com>
2022-11-20 20:04:44 +0100

    Update test-runner

--- test-runner/src/main.rs
+++ test-runner/src/main.rs
@@ -60,11 +60,19 @@ fn run_test_definition(
     println!("------------ Test Definition ------------");
     println!("{}", test_definition.description);
 
+    for test in test_definition.tests {
+        run_test(&test, scripts_dir.clone(), compiler_path.clone());
+    }
+
+    Ok(())
+}
+
+fn run_test(test: &json::Test, scripts_dir: PathBuf, compiler_path: PathBuf) {
     let count = thread::available_parallelism()
         .map(|x| x.get())
         .unwrap_or(1_usize);
 
-    let chunks: Vec<&[json::Test]> = test_definition.tests.chunks(count).collect();
+    let chunks: Vec<&[json::Script]> = test.scripts.chunks(count).collect();
 
     let multi_progress = MultiProgress::new();
 
@@ -77,20 +85,19 @@ fn run_test_definition(
     for chunk in chunks {
         let scripts_dir = scripts_dir.clone();
         let compiler_path = compiler_path.clone();
-        let tests = chunk.to_vec();
+        let chunk = chunk.to_vec();
 
-        let pb = multi_progress.add(ProgressBar::new(tests.len() as u64));
+        let pb = multi_progress.add(ProgressBar::new(chunk.len() as u64));
         pb.set_style(spinner_style.clone());
 
         handles.push(thread::spawn(move || {
             let scripts_dir = scripts_dir;
             let compiler_path = compiler_path;
-            let tests = tests;
 
-            for test in tests {
-                pb.set_message(test.name.to_string());
+            for script in chunk {
+                pb.set_message(script.file.to_string());
                 pb.inc(1);
-                run_test(&test, &scripts_dir, &compiler_path);
+                compile_script(&script, &scripts_dir, &compiler_path);
             }
 
             pb.finish_with_message("waiting...");
@@ -101,42 +108,40 @@ fn run_test_definition(
         handle.join().unwrap();
     }
 
-    Ok(())
+    multi_progress.clear().unwrap();
 }
 
-fn run_test(test: &json::Test, scripts_dir: &Path, compiler_path: &Path) {
-    for script in &test.scripts {
-        let script_path = scripts_dir.join(&script.file);
-        if !script_path.exists() {
-            println!("Script {} does not exist!", script_path.display());
-            continue;
-        }
+fn compile_script(script: &json::Script, scripts_dir: &Path, compiler_path: &Path) {
+    let script_path = scripts_dir.join(&script.file);
+    if !script_path.exists() {
+        println!("Script {} does not exist!", script_path.display());
+        return;
+    }
 
-        let script_name = script_path
-            .file_stem()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_owned();
+    let script_name = script_path
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_owned();
 
-        let import_dir = script_path.parent().unwrap().to_owned();
+    let import_dir = script_path.parent().unwrap().to_owned();
 
-        let process_output = std::process::Command::new(compiler_path)
-            .stdin(std::process::Stdio::null())
-            .arg(script_name)
-            .arg("-o=out")
-            .arg(format!("-i={}", import_dir.display()))
-            .output()
-            .unwrap();
+    let process_output = std::process::Command::new(compiler_path)
+        .stdin(std::process::Stdio::null())
+        .arg(script_name)
+        .arg("-o=out")
+        .arg(format!("-i={}", import_dir.display()))
+        .output()
+        .unwrap();
 
-        let result_success = process_output.status.success();
-        let expected_success = script.expected_result == json::ScriptResult::Success;
-        let test_success = result_success == expected_success;
+    let result_success = process_output.status.success();
+    let expected_success = script.expected_result == json::ScriptResult::Success;
+    let test_success = result_success == expected_success;
 
-        if !test_success {
-            println!("Test failed");
-            io::stdout().write_all(&process_output.stdout).unwrap();
-            io::stderr().write_all(&process_output.stderr).unwrap();
-        }
+    if !test_success {
+        println!("Test failed");
+        io::stdout().write_all(&process_output.stdout).unwrap();
+        io::stderr().write_all(&process_output.stderr).unwrap();
     }
 }
